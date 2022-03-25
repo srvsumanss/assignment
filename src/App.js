3730a64ce8b750ea3370a2f7d0ae8db13b27d7fc
srvsumanss <srvsumanss@gmail.com>
2022-03-26 00:34:56 +0530

    form

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React from 'react';
+import Form from './components/Forms';
+import './App.css';
+
+const App = ()=> {
+  return (
+    
+     
+<Form />
+     
+    
+  );
+}
+
+export default App;
